e18b99ac24ab90133a9c5eb06e92a6ed77b2cb32
backSeungWook <tmxk3856@gmail.com>
2024-01-03 15:53:01 +0900

    init

---
+++ src/main/java/com/fastcampus/projectboard/ProjectboardApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.projectboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectboardApplication {
+	//Start
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectboardApplication.class, args);
+	}
+
+}
