f3abe4436ac245408a848e87332e0b5a507eb511
backSeungWook <tmxk3856@gmail.com>
2024-01-14 22:00:43 +0900

    Part2-3-14-16

--- src/main/java/com/fastcampus/projectboard/config/SecuityConfig.java
+++ src/main/java/com/fastcampus/projectboard/config/SecuityConfig.java
@@ -2,6 +2,7 @@ package com.fastcampus.projectboard.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -9,14 +10,23 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecuityConfig {
 
+//    antMatchers == mvcMatchers
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll()) // 전체 다 허용
+                .authorizeHttpRequests(auth -> auth
+                        .mvcMatchers(
+                                HttpMethod.GET,
+                                "/",
+                                "articles",
+                                "/articles/search-hashtag"
+                        ).permitAll()
+                        .anyRequest().authenticated()
+
+//                        .anyRequest().permitAll() // 전체 다 허용
+                )
                 .formLogin().and()
+                .csrf().disable()
                 .build();
-
-
-
     }
 }
